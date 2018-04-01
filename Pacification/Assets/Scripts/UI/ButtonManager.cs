a721be043d19e5714ed90888546327371e944b85
Valerian <valerian.fayt@epita.fr>
2018-04-01 19:40:10 +0200

    add cheatmode editor

--- Pacification/Assets/Scripts/UI/ButtonManager.cs
+++ Pacification/Assets/Scripts/UI/ButtonManager.cs
@@ -5,6 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour {
 
+    public GameObject leftPannel;
+    public GameObject rightPannel;
+
+    private bool cheatmode = false;
+    private float time = 0F;
+
+    public void Update()
+    {
+        if(Input.GetKey(KeyCode.Tab) && Time.time >= time)
+        {
+            if(cheatmode)
+            {
+                Debug.Log("CheatMode deactivated");
+                leftPannel.SetActive(false);
+                rightPannel.SetActive(false);
+                cheatmode = false;
+            }
+            else
+            {
+                Debug.Log("CheatMode activated");
+                leftPannel.SetActive(true);
+                rightPannel.SetActive(true);
+                cheatmode = true;
+            }
+            time = Time.time + 0.5F;
+        }
+    }
+
     public void DeconnectionButton()
     {
         Server server = FindObjectOfType<Server>();

03ae3aea601968fe99fbabc1f34b16263db6b002
emrayce <cedric.parpet@orange.fr>
2018-04-12 05:22:47 +0200

    Add the new menu but kept the ancient
    one bug : Waiting panel doesn't appear
    Kept the ancient menu to have one fully operational

---
+++ Pacification/Assets/Scripts/UI/LUI_PressAnyKey.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class LUI_PressAnyKey : MonoBehaviour {
+
+	[Header("OBJECTS")]
+	public GameObject scriptObject;
+	public Animator animatorComponent;
+
+	void Start ()
+	{
+		animatorComponent.GetComponent<Animator>();
+	}
+
+	void Update ()
+	{
+		if (Input.anyKeyDown) 
+		{
+			animatorComponent.Play ("Bloody Splash Anim");
+			Destroy (scriptObject);
+		}
+	}
+}

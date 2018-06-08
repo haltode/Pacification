80f3c301bdb1a9efd2d711a78e33e55b76077e86
haltode <tallancon@gmail.com>
2018-06-08 18:41:29 +0200

    unit: reduce science required to embark

--- Pacification/Assets/Scripts/Units/Unit.cs
+++ Pacification/Assets/Scripts/Units/Unit.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Unit
 {
-    public const int ScienceMinLevelEmbark = 2000;
+    public const int ScienceMinLevelEmbark = 150;
 
     protected HexUnit hexUnit;
     public GameObject hexGameObject;

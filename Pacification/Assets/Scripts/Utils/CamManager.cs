b491db2f2c83f45d09bd4306f7a2c06172035de9
Valerian <valerian.fayt@epita.fr>
2018-06-27 19:21:44 +0200

    dev: add cinematic cam

---
+++ Pacification/Assets/Scripts/Utils/CamManager.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamManager : MonoBehaviour
+{
+
+    public GameObject cameraOne;
+    public GameObject cameraTwo;
+
+    int cameraCount = -1;
+
+    void Start()
+    {
+        CameraChangeCounter();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F12))
+            CameraChangeCounter();
+    }
+
+    //Camera Counter
+    void CameraChangeCounter()
+    {
+        //int cameraPositionCounter = PlayerPrefs.GetInt("CameraPosition"); //Will register wich cam was last chosen for the next game 
+        cameraCount++;
+        if(cameraCount > 1)
+            cameraCount = 0;
+        //PlayerPrefs.SetInt("CameraPosition", camPosition);
+
+        if(cameraCount == 0)
+        {
+            cameraOne.SetActive(true);
+            cameraTwo.SetActive(false);
+        }
+        else
+        {
+            cameraTwo.SetActive(true);
+            cameraOne.SetActive(false);
+        }
+    }
+}
---
+++ Pacification/Assets/Scripts/Utils/FreeCamMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeCamMovement : MonoBehaviour {
+
+    Camera mycam;
+
+
+    void Start()
+    {
+        mycam = GetComponent<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float sensitivity = 0.05f;
+        Vector3 vp = mycam.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mycam.nearClipPlane));
+        vp.x -= 0.5f;
+        vp.y -= 0.5f;
+        vp.x *= sensitivity;
+        vp.y *= sensitivity;
+        vp.x += 0.5f;
+        vp.y += 0.5f;
+        Vector3 sp = mycam.ViewportToScreenPoint(vp);
+        Vector3 v = mycam.ScreenToWorldPoint(sp);
+        transform.LookAt(v, Vector3.up);
+    }
+}
